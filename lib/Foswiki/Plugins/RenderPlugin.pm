832952b59333b845519168164c18cff270d3d72c
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-09 08:13:39 +0000

    Item2017:    * added template REST handler    * removed "parked" upload REST handler - moved & improved version in UploadPlugin
    git-svn-id: http://svn.foswiki.org/trunk/RenderPlugin@4818 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/RenderPlugin.pm
+++ lib/Foswiki/Plugins/RenderPlugin.pm
@@ -22,7 +22,7 @@ use strict;
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $NO_PREFS_IN_TOPIC );
 
 $VERSION = '$Rev$';
-$RELEASE = '2.0';
+$RELEASE = '3.0';
 
 $SHORTDESCRIPTION = 'Render <nop>WikiApplications asynchronously';
 $NO_PREFS_IN_TOPIC = 1;
@@ -40,9 +40,9 @@ sub initPlugin {
   my ($topic, $web, $user, $installWeb) = @_;
 
   Foswiki::Func::registerRESTHandler('tag', \&restTag);
+  Foswiki::Func::registerRESTHandler('template', \&restTemplate);
   Foswiki::Func::registerRESTHandler('expand', \&restExpand);
   Foswiki::Func::registerRESTHandler('render', \&restRender);
-  Foswiki::Func::registerRESTHandler('upload', \&restUpload);
 
   return 1;
 }
@@ -76,6 +76,40 @@ sub restExpand {
 
   # and render it
   return Foswiki::Func::expandCommonVariables($theText, $topic, $web) || ' ';
+}
+
+###############################################################################
+sub restTemplate {
+  my ($session, $subject, $verb) = @_;
+
+  my $query = Foswiki::Func::getCgiQuery();
+  my $theTemplate = $query->param('name');
+  return '' unless $theTemplate;
+
+  my $theExpand = $query->param('expand');
+  return '' unless $theExpand;
+
+  my $theRender = $query->param('render') || 0;
+
+  $theRender = ($theRender =~ /^\s*(1|on|yes|true)\s*$/) ? 1:0;
+  my $theTopic = $query->param('topic') || $session->{topicName};
+  my $theWeb = $query->param('web') || $session->{webName};
+  my ($web, $topic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+
+  Foswiki::Func::loadTemplate($theTemplate);
+
+  require Foswiki::Attrs;
+  my $attrs = new Foswiki::Attrs($theExpand);
+
+  my $tmpl = $session->templates->tmplP($attrs);
+
+  # and render it
+  my $result = Foswiki::Func::expandCommonVariables($tmpl, $topic, $web) || ' ';
+  if ($theRender) {
+    $result = Foswiki::Func::renderText($result, $web);
+  }
+
+  return $result;
 }
 
 ###############################################################################
@@ -120,95 +154,6 @@ sub restTag {
   #writeDebug("result=$result");
 
   return $result;
-}
-
-###############################################################################
-sub restUpload {
-  my ($session, $subject, $verb) = @_;
-
-  my $query = Foswiki::Func::getCgiQuery();
-  my $topic = $query->param('topic');
-  my $web;
-
-  ($web, $topic) = Foswiki::Func::normalizeWebTopicName("", $topic);
-
-  my $hideFile = $query->param('hidefile') || '';
-  my $fileComment = $query->param('filecomment') || '';
-  my $createLink = $query->param('createlink') || '';
-  my $doPropsOnly = $query->param('changeproperties');
-  my $filePath = $query->param('filepath') || '';
-  my $fileName = $query->param('filename') || '';
-  if ($filePath && ! $fileName) {
-      $filePath =~ m|([^/\\]*$)|;
-      $fileName = $1;
-  }
-  $fileComment =~ s/\s+/ /go;
-  $fileComment =~ s/^\s*//o;
-  $fileComment =~ s/\s*$//o;
-  $fileName =~ s/\s*$//o;
-  $filePath =~ s/\s*$//o;
-
-  unless (Foswiki::Func::checkAccessPermission(
-      'CHANGE', Foswiki::Func::getWikiName(), undef, $topic, $web)) {
-      return "Access denied";
-  }
-
-  my ($fileSize, $fileDate, $tmpFileName);
-
-  my $stream = $query->upload('filepath') unless $doPropsOnly;
-  my $origName = $fileName;
-
-  unless($doPropsOnly) {
-      # SMELL: call to unpublished function
-      ($fileName, $origName) =
-        Foswiki::Sandbox::sanitizeAttachmentName($fileName);
-
-      # check if upload has non zero size
-      if($stream) {
-          my @stats = stat $stream;
-          $fileSize = $stats[7];
-          $fileDate = $stats[9];
-      }
-
-      unless($fileSize && $fileName) {
-          return "Zero-sized file upload";
-      }
-
-      my $maxSize = Foswiki::Func::getPreferencesValue(
-          'ATTACHFILESIZELIMIT');
-      $maxSize = 0 unless ($maxSize =~ /([0-9]+)/o);
-
-      if ($maxSize && $fileSize > $maxSize * 1024) {
-          return "Oversized upload";
-      }
-  }
-
-  # SMELL: use of undocumented CGI::tmpFileName
-  my $tfp = $query->tmpFileName($query->param('filepath'));
-  my $dontlog = $Foswiki::cfg{Log}{upload};
-  $dontlog = $Foswiki::cfg{Log}{upload} unless defined $dontlog;
-  my $error = Foswiki::Func::saveAttachment(
-      $web, $topic, $fileName,
-      {
-          dontlog => !$dontlog,
-          comment => $fileComment,
-          hide => $hideFile,
-          createlink => $createLink,
-          stream => $stream,
-          filepath => $filePath,
-          filesize => $fileSize,
-          filedate => $fileDate,
-          tmpFilename => $tfp,
-      });
-
-  close($stream) if $stream;
-
-  return $error if $error;
-
-  # Otherwise allow the rest dispatcher to write a 200
-  return 
-    "$origName attached to $web.$topic" . 
-    ($origName ne $fileName ?" as $fileName" : '');
 }
 
 1;
