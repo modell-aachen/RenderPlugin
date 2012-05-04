812d5261c675383cfa4190dd84c74a81b91a9fde
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/RenderPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/RenderPlugin.pm
+++ lib/Foswiki/Plugins/RenderPlugin.pm
@@ -19,141 +19,148 @@ use strict;
 use warnings;
 
 use Foswiki::Func ();
-use Foswiki::Sandbox() ;
+use Foswiki::Sandbox();
 use Encode ();
 
-our $VERSION = '$Rev$';
-our $RELEASE = '3.1';
-our $SHORTDESCRIPTION = 'Render <nop>WikiApplications asynchronously';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '3.1';
+our $SHORTDESCRIPTION  = 'Render <nop>WikiApplications asynchronously';
 our $NO_PREFS_IN_TOPIC = 1;
 
-use constant DEBUG => 0; # toggle me
+use constant DEBUG => 0;    # toggle me
 
 ###############################################################################
 sub writeDebug {
-  print STDERR '- RenderPlugin - '.$_[0]."\n" if DEBUG;
+    print STDERR '- RenderPlugin - ' . $_[0] . "\n" if DEBUG;
 }
-
 
 ###############################################################################
 sub initPlugin {
-  my ($topic, $web, $user, $installWeb) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
-  Foswiki::Func::registerRESTHandler('tag', \&restTag);
-  Foswiki::Func::registerRESTHandler('template', \&restTemplate);
-  Foswiki::Func::registerRESTHandler('expand', \&restExpand);
-  Foswiki::Func::registerRESTHandler('render', \&restRender);
+    Foswiki::Func::registerRESTHandler( 'tag',      \&restTag );
+    Foswiki::Func::registerRESTHandler( 'template', \&restTemplate );
+    Foswiki::Func::registerRESTHandler( 'expand',   \&restExpand );
+    Foswiki::Func::registerRESTHandler( 'render',   \&restRender );
 
-  return 1;
+    return 1;
 }
 
 ###############################################################################
 sub restRender {
-  my ($session, $subject, $verb) = @_;
+    my ( $session, $subject, $verb ) = @_;
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $theTopic = $query->param('topic') || $session->{topicName};
-  my $theWeb = $query->param('web') || $session->{webName};
-  my ($web, $topic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+    my $query    = Foswiki::Func::getCgiQuery();
+    my $theTopic = $query->param('topic') || $session->{topicName};
+    my $theWeb   = $query->param('web') || $session->{webName};
+    my ( $web, $topic ) =
+      Foswiki::Func::normalizeWebTopicName( $theWeb, $theTopic );
 
-  return Foswiki::Func::renderText(restExpand($session, $subject, $verb), $web);
+    return Foswiki::Func::renderText( restExpand( $session, $subject, $verb ),
+        $web );
 }
 
 ###############################################################################
 sub restExpand {
-  my ($session, $subject, $verb) = @_;
+    my ( $session, $subject, $verb ) = @_;
 
-  # get params
-  my $query = Foswiki::Func::getCgiQuery();
-  my $theText = $query->param('text') || '';
+    # get params
+    my $query = Foswiki::Func::getCgiQuery();
+    my $theText = $query->param('text') || '';
 
-  return ' ' unless $theText; # must return at least on char as we get a
-                              # premature end of script otherwise
+    return ' ' unless $theText;    # must return at least on char as we get a
+                                   # premature end of script otherwise
 
-  my $theTopic = $query->param('topic') || $session->{topicName};
-  my $theWeb = $query->param('web') || $session->{webName};
-  my ($web, $topic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+    my $theTopic = $query->param('topic') || $session->{topicName};
+    my $theWeb   = $query->param('web')   || $session->{webName};
+    my ( $web, $topic ) =
+      Foswiki::Func::normalizeWebTopicName( $theWeb, $theTopic );
 
-  # and render it
-  return Foswiki::Func::expandCommonVariables($theText, $topic, $web) || ' ';
+    # and render it
+    return Foswiki::Func::expandCommonVariables( $theText, $topic, $web )
+      || ' ';
 }
 
 ###############################################################################
 sub restTemplate {
-  my ($session, $subject, $verb) = @_;
+    my ( $session, $subject, $verb ) = @_;
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $theTemplate = $query->param('name');
-  return '' unless $theTemplate;
+    my $query       = Foswiki::Func::getCgiQuery();
+    my $theTemplate = $query->param('name');
+    return '' unless $theTemplate;
 
-  my $theExpand = $query->param('expand');
-  return '' unless $theExpand;
+    my $theExpand = $query->param('expand');
+    return '' unless $theExpand;
 
-  my $theTopic = $query->param('topic') || $session->{topicName};
-  my $theWeb = $query->param('web') || $session->{webName};
-  my ($web, $topic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+    my $theTopic = $query->param('topic') || $session->{topicName};
+    my $theWeb   = $query->param('web')   || $session->{webName};
+    my ( $web, $topic ) =
+      Foswiki::Func::normalizeWebTopicName( $theWeb, $theTopic );
 
-  Foswiki::Func::loadTemplate($theTemplate);
+    Foswiki::Func::loadTemplate($theTemplate);
 
-  require Foswiki::Attrs;
-  my $attrs = new Foswiki::Attrs($theExpand);
+    require Foswiki::Attrs;
+    my $attrs = new Foswiki::Attrs($theExpand);
 
-  my $tmpl = $session->templates->tmplP($attrs);
+    my $tmpl = $session->templates->tmplP($attrs);
 
-  # and render it
-  my $result = Foswiki::Func::expandCommonVariables($tmpl, $topic, $web) || ' ';
+    # and render it
+    my $result = Foswiki::Func::expandCommonVariables( $tmpl, $topic, $web )
+      || ' ';
 
-  my $theRender = Foswiki::Func::isTrue($query->param('render'),  0);
-  if ($theRender) {
-    $result = Foswiki::Func::renderText($result, $web);
-  }
+    my $theRender = Foswiki::Func::isTrue( $query->param('render'), 0 );
+    if ($theRender) {
+        $result = Foswiki::Func::renderText( $result, $web );
+    }
 
-  return $result;
+    return $result;
 }
 
 ###############################################################################
 sub restTag {
-  my ($session, $subject, $verb) = @_;
+    my ( $session, $subject, $verb ) = @_;
 
-  #writeDebug("called restTag($subject, $verb)");
+    #writeDebug("called restTag($subject, $verb)");
 
-  # get params
-  my $query = Foswiki::Func::getCgiQuery();
+    # get params
+    my $query = Foswiki::Func::getCgiQuery();
 
-  my $theTag = $query->param('name') || 'INCLUDE';
-  my $theDefault = $query->param('param') || '';
-  my $theRender = $query->param('render') || 0;
+    my $theTag     = $query->param('name')   || 'INCLUDE';
+    my $theDefault = $query->param('param')  || '';
+    my $theRender  = $query->param('render') || 0;
 
-  $theRender = ($theRender =~ /^\s*(1|on|yes|true)\s*$/) ? 1:0;
+    $theRender = ( $theRender =~ /^\s*(1|on|yes|true)\s*$/ ) ? 1 : 0;
 
-  my $theTopic = $query->param('topic') || $session->{topicName};
-  my $theWeb = $query->param('web') || $session->{webName};
-  my ($web, $topic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+    my $theTopic = $query->param('topic') || $session->{topicName};
+    my $theWeb   = $query->param('web')   || $session->{webName};
+    my ( $web, $topic ) =
+      Foswiki::Func::normalizeWebTopicName( $theWeb, $theTopic );
 
-  # construct parameters for tag
-  my $params = $theDefault?'"'.$theDefault.'"':'';
-  foreach my $key ($query->param()) {
-    next if $key =~ /^(name|param|render|topic|XForms:Model)$/;
-    my $value = $query->param($key);
-    $params .= ' '.$key.'="'.$value.'" ';
-  }
+    # construct parameters for tag
+    my $params = $theDefault ? '"' . $theDefault . '"' : '';
+    foreach my $key ( $query->param() ) {
+        next if $key =~ /^(name|param|render|topic|XForms:Model)$/;
+        my $value = $query->param($key);
+        $params .= ' ' . $key . '="' . $value . '" ';
+    }
 
-  # create TML expression
-  my $tml = '%'.$theTag;
-  $tml .= '{'.$params.'}' if $params;
-  $tml .= '%';
+    # create TML expression
+    my $tml = '%' . $theTag;
+    $tml .= '{' . $params . '}' if $params;
+    $tml .= '%';
 
-  #writeDebug("tml=$tml");
+    #writeDebug("tml=$tml");
 
-  # and render it
-  my $result = Foswiki::Func::expandCommonVariables($tml, $topic, $web) || ' ';
-  if ($theRender) {
-    $result = Foswiki::Func::renderText($result, $web);
-  }
+    # and render it
+    my $result = Foswiki::Func::expandCommonVariables( $tml, $topic, $web )
+      || ' ';
+    if ($theRender) {
+        $result = Foswiki::Func::renderText( $result, $web );
+    }
 
-  #writeDebug("result=$result");
+    #writeDebug("result=$result");
 
-  return $result;
+    return $result;
 }
 
 1;
